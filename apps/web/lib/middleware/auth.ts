eb1f30bd533a6cd0c199d5a54b1bd1bdb7973b72
Suprim Khatri <Supremekhatri62@gmail.com>
2026-05-05 13:23:16 +0545

    hardcoded cookie domain in auth.ts middleware temporarily

--- apps/web/lib/middleware/auth.ts
+++ apps/web/lib/middleware/auth.ts
@@ -41,14 +41,14 @@ export async function attemptRefresh(
       redirect.cookies.set("access_token", "", {
         maxAge: 0,
         path: "/",
-        domain: process.env.COOKIE_DOMAIN,
+        domain: ".sms.suprimkhatri.com.np",
         secure: true,
         httpOnly: true,
       });
       redirect.cookies.set("refresh_token", "", {
         maxAge: 0,
         path: "/",
-        domain: process.env.COOKIE_DOMAIN,
+        domain: ".sms.suprimkhatri.com.np",
         secure: true,
         httpOnly: true,
       });
