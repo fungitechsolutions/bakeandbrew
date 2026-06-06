49dcba06a71dd3346e6953fa09289e76ee6fb58e
Suprim Khatri <Supremekhatri62@gmail.com>
2026-06-06 14:43:07 +0545

    feat: docker multistage build setup

--- apps/web/lib/api-url.ts
+++ apps/web/lib/api-url.ts
@@ -1,5 +1,5 @@
 export function getApiUrl() {
-  return process.env.NODE_ENV !== "production" && typeof window === "undefined"
+  return typeof window === "undefined" && process.env.INTERNAL_API_URL
     ? process.env.INTERNAL_API_URL
     : process.env.NEXT_PUBLIC_API_URL;
 }
